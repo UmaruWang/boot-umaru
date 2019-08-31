45167661c1e35b4a1764b5c20e58ef1a2a216101
UmaruWang <332416146@qq.com>
2019-08-31 13:29:59 +0800

    init

---
+++ src/main/java/com/umaru/bootumaru/BootUmaruApplication.java
@@ -0,0 +1,13 @@
+package com.umaru.bootumaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootUmaruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootUmaruApplication.class, args);
+    }
+
+}
